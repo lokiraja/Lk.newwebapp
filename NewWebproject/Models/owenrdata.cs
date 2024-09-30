99e3084f7453cd1101428132e46a5d3dfbca23f3
lokiraja <lokesh2kumar9@outlook.com>
2024-09-30 21:13:34 +0530

    add a new model class

---
+++ NewWebproject/Models/owenrdata.cs
@@ -0,0 +1,10 @@
+﻿namespace NewWebproject.Models
+{
+    public class owenrdata
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Description { get; set; } 
+        public string Type { get; set; }
+    }
+}
